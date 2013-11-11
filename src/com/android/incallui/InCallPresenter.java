3d381b5d67ef06b6f94c9bb7b48f898ce7c46663
Sandeep Gutta <sangutta@codeaurora.org>
2013-11-11 19:23:13 +0530

    DSDA: Add InCallUI DSDA support.
     -Add tab view support for DSDA
     -Add support to display voice calls based on the
      current active subscription.
     -Add few utilities in CallList, which required for handling
      voice calls across multiple subscription.
    
    Change-Id: Ib683f7c3b41ed3bb04367be1e9c331908bc46004

--- src/com/android/incallui/InCallPresenter.java
+++ src/com/android/incallui/InCallPresenter.java
@@ -20,6 +20,8 @@
 
 package com.android.incallui;
 
+import android.telephony.MSimTelephonyManager;
+
 import com.android.incallui.service.PhoneNumberService;
 import com.google.android.collect.Sets;
 import com.google.common.base.Preconditions;
@@ -183,6 +185,11 @@ public class InCallPresenter implements CallList.Listener {
 
         final boolean doFinish = (mInCallActivity != null && isActivityStarted());
         Log.i(this, "Hide in call UI: " + doFinish);
+
+        if ((mCallList != null) && !(mCallList.existsLiveCall(mCallList.getActiveSubscription()))
+                && mCallList.switchToOtherActiveSubscription()) {
+            return;
+        }
 
         if (doFinish) {
             mInCallActivity.finish();
@@ -362,6 +369,11 @@ public class InCallPresenter implements CallList.Listener {
             listener.onStateChange(mInCallState, callList);
         }
 
+        if (MSimTelephonyManager.getDefault().getMultiSimConfiguration()
+                == MSimTelephonyManager.MultiSimVariants.DSDA && (mInCallActivity != null)) {
+            mInCallActivity.updateDsdaTab();
+        }
+
         if (isActivityStarted()) {
             final boolean hasCall = callList.getActiveOrBackgroundCall() != null ||
                     callList.getOutgoingCall() != null;
@@ -396,6 +408,11 @@ public class InCallPresenter implements CallList.Listener {
 
         for (IncomingCallListener listener : mIncomingCallListeners) {
             listener.onIncomingCall(mInCallState, call);
+        }
+
+        if (MSimTelephonyManager.getDefault().getMultiSimConfiguration()
+                == MSimTelephonyManager.MultiSimVariants.DSDA && (mInCallActivity != null)) {
+            mInCallActivity.updateDsdaTab();
         }
     }
 
@@ -880,7 +897,12 @@ public class InCallPresenter implements CallList.Listener {
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
                 | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
                 | Intent.FLAG_ACTIVITY_NO_USER_ACTION);
-        intent.setClass(mContext, InCallActivity.class);
+        if (MSimTelephonyManager.getDefault().getMultiSimConfiguration()
+                == MSimTelephonyManager.MultiSimVariants.DSDA) {
+            intent.setClass(mContext, MSimInCallActivity.class);
+        } else {
+            intent.setClass(mContext, InCallActivity.class);
+        }
         if (showDialpad) {
             intent.putExtra(InCallActivity.SHOW_DIALPAD_EXTRA, true);
         }
